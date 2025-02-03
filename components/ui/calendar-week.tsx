7ca5adcb6a2f66e8f7f4ae1aa567cf71ba8b31b9
acodingfish <jonmd@hotmail.com>
2025-02-02 18:10:29 -0600

    minor refactor.

--- components/ui/calendar-week.tsx
+++ components/ui/calendar-week.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren, ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Fragment } from 'react';
+import React, { Fragment, useMemo} from 'react';
 
 import PaletteColor, {ColorElement, ColorTheme} from "@/components/ui/color-manager"
 import CalendarDay, {GetWeekDay, IsLeapYear} from './calendar-day';
@@ -29,38 +29,52 @@ const styles = StyleSheet.create({
 
 export default function CalendarWeek({children, year, month, day}:Props) {
 
-    let week_index = GetWeekDay(year, month, day);
-    let week = [];
-    var render_week = <div></div>;
-    for (let  i = 0; i < 7; i++)
-    {
-        let date = {year:year, month:month, day:day};
-        if (i<week_index) {
-            for (let j = i; j < week_index; j++)        // For days earlier in the week
-            {
-                date = GetPrevDate(date);
-            }
-        } else if (i>week_index) {
-            for (let j = i; j > week_index; j--)        // for days later in the week
-            {
-                date = GetNextDate(date);
-            }
-        } // else today's date
-        // week.push(date);
-        week.push(CreateWeekday(children,date))
-    }
+    
+    let date = {year:year, month:month, day:day};
+    // let week = [];
+    // week=CreateWeek(children, date);
+
+    const week = useMemo(() =>{
+        return CreateWeek(date);
+    },[year, month, day]);
+    
 
     return (
         <View style={[styles.content,{backgroundColor:PaletteColor(ColorElement.HEADER_BACKGROUND)}]}>
-            {/* { week.map((date) => { return CreateWeekday(children, date)})} */}
+            { week.map((date) => { return CreateWeekday(children, date)})}
             {/* { week.map(({year, month, day}) =>  CreateWeekday(children, {year, month, day}))} */}
-            {week}
+            {/* {CreateWeek(children, date)} */}
+            {/* {week} */}
         </View>
     );
 }
 
+function CreateWeek(date:{year:number, month:number, day:number})
+{    
+    let week_index = GetWeekDay(date.year, date.month, date.day);
+    let week = [];
+    for (let  i = 0; i < 7; i++)
+    {
+        let tmp_date = date;
+        if (i<week_index) {
+            for (let j = i; j < week_index; j++)        // For days earlier in the week
+            {
+                tmp_date = GetPrevDate(tmp_date);
+            }
+        } else if (i>week_index) {
+            for (let j = i; j > week_index; j--)        // for days later in the week
+            {
+                tmp_date = GetNextDate(tmp_date);
+            }
+        } // else today's date
+        week.push(tmp_date);
+        // week.push(CreateWeekday(children,tmp_date))
+    }
+    return (week);
+}
 
-function CreateWeekday(children:any, date:{year:number, month:number, day:number})
+
+function CreateWeekday(children:React.ReactNode, date:{year:number, month:number, day:number})
 {
     return (
         <CalendarDay children={children} year={date.year} month={date.month} day={date.day}></CalendarDay>
